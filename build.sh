c08f4947571f441ee1462f884ea21c7962e7dedd
Jakub Skowron <jakubskowron676@gmail.com>
2024-10-09 11:52:30 +0200

    Copy `compile_commands.json`

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ cmake --build build/linux_x64-Debug/ -j4
 
 cp build/linux_x64-Debug/libTIMGE.a ../Sandbox/lib/
 cp -r build/linux_x64-Debug/include/TIMGE/ ../Sandbox/include/
+cp build/linux_x64-Debug/compile_commands.json ./
 
 cd ../Sandbox/
 
@@ -16,5 +17,7 @@ mkdir build/
 
 cmake -S . -B build/linux_x64-Debug/
 cmake --build build/linux_x64-Debug/ -j4
+
+cp build/linux_x64-Debug/compile_commands.json ./
 
 build/linux_x64-Debug/TIMGESandbox

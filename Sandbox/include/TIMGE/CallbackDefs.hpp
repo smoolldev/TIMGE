2ddbfd0b8782341ff098f3fe24d267c1c8e3f3d3
Jakub Skowron <jakubskowron676@gmail.com>
2024-10-21 19:03:14 +0200

    Fix `Sandbox/src/main.cpp`

---
+++ Sandbox/include/TIMGE/CallbackDefs.hpp
@@ -0,0 +1,101 @@
+#ifndef CALLBACKDEFS_HPP
+#define CALLBACKDEFS_HPP
+
+#include "TIMGE/Keyboard.hpp"
+#include "TIMGE/Mouse.hpp"
+#include "TIMGE/Utils/Vector.hpp"
+
+#include <string_view>
+
+#include <GLFW/glfw3.h>
+
+namespace TIMGE::Callback
+{
+    using Error_t = void (*)(int errorCode, std::string_view description);
+    using WindowPos_t = void (*)(const V2i32& position);
+    using WindowSize_t = void (*)(const V2i32& size);
+    using WindowClose_t = void (*)();
+    using WindowRefresh_t = void (*)();
+    using WindowFocus_t = void (*)(bool focused);
+    using WindowIconify_t = void (*)(bool iconified);
+    using WindowMaximize_t = void (*)(bool maximized);
+    using FramebufferSize_t = void (*)(const V2i32& framebufferSize);
+    using WindowContentScale_t = void (*)(const V2f& contentScale);
+    using MouseButton_t = void (*)(Button button, Mouse::Action action, Modifier mods);
+    using CursorPos_t = void (*)(const V2d& cursorPosition);
+    using CursorEnter_t = void (*)(bool entered);
+    using Scroll_t = void (*)(const V2d& cursorScrollOffset);
+    using Key_t = void (*)(Key key, int scancode, Keyboard::Action action, Modifier mods);
+    using Char_t = void (*)(unsigned int codepoint);
+    using Charmods_t = void (*)(unsigned int codepoint, int mods);
+    using Drop_t = void (*)(int pathCount, const char* paths[]);
+    using Monitor_t = void (*)(int event);
+    using Joystick_t = void (*)(int jid, int event);
+
+    using timgeError_t = void (*)(int errorCode, const char* description);
+    using timgeWindowPos_t = void (*)(GLFWwindow* window, int xPos, int yPos);
+    using timgeWindowSize_t = void (*)(GLFWwindow* window, int width, int height);
+    using timgeWindowClose_t = void (*)(GLFWwindow* window);
+    using timgeWindowRefresh_t = void (*)(GLFWwindow* window);
+    using timgeWindowFocus_t = void (*)(GLFWwindow* window, int focused);
+    using timgeWindowIconify_t = void (*)(GLFWwindow* window, int iconified);
+    using timgeWindowMaximize_t = void (*)(GLFWwindow* window, int maximized);
+    using timgeFramebufferSize_t = void (*)(GLFWwindow* window, int width, int height);
+    using timgeWindowContentScale_t = void (*)(GLFWwindow* window, float xScale, float yScale);
+    using timgeMouseButton_t = void (*)(GLFWwindow* window, int button, int action, int mods);
+    using timgeCursorPos_t = void (*)(GLFWwindow* window, double xPos, double yPos);
+    using timgeCursorEnter_t = void (*)(GLFWwindow* window, int entered);
+    using timgeScroll_t = void (*)(GLFWwindow* window, double xOffset, double yOffset);
+    using timgeKey_t = void (*)(GLFWwindow* window, int key, int scancode, int action, int mods);
+    using timgeChar_t = void (*)(GLFWwindow* window, unsigned int codepoint);
+    using timgeCharmods_t = void (*)(GLFWwindow* window, unsigned int codepoint, int mods);
+    using timgeDrop_t = void (*)(GLFWwindow* window, int pathCount, const char* paths[]);
+    using timgeMonitor_t = void (*)(GLFWmonitor* monitor, int event);
+    using timgeJoystick_t = void (*)(int jid, int event);
+
+    void ErrorCallback(int errorCode, const char* description);
+    void WindowPosCallback(GLFWwindow* window, int xPos, int yPos);
+    void WindowSizeCallback(GLFWwindow* window, int width, int height);
+    void WindowCloseCallback(GLFWwindow* window);
+    void WindowRefreshCallback(GLFWwindow* window);
+    void WindowFocusCallback(GLFWwindow* window, int focused);
+    void WindowIconifyCallback(GLFWwindow* window, int iconified);
+    void WindowMaximizeCallback(GLFWwindow* window, int maximized);
+    void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+    void WindowContentScaleCallback(GLFWwindow* window, float xScale, float yScale);
+    void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+    void CursorPosCallback(GLFWwindow* window, double xPos, double yPos);
+    void CursorEnterCallback(GLFWwindow* window, int entered);
+    void ScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+    void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+    void CharCallback(GLFWwindow* window, unsigned int codepoint);
+    void CharModsCallback(GLFWwindow* window, unsigned int codepoint, int mods);
+    void DropCallback(GLFWwindow* window, int pathCount, const char* path[]);
+    void MonitorCallback(GLFWmonitor* monitor, int event);
+    void JoystickCallback(int jid, int event);
+
+    struct Callbacks
+    {
+        Error_t mError;
+        WindowPos_t mWindowPos;
+        WindowSize_t mWindowSize;
+        WindowClose_t mWindowClose;
+        WindowRefresh_t mWindowRefresh;
+        WindowFocus_t mWindowFocus;
+        WindowIconify_t mWindowIconify;
+        WindowMaximize_t mWindowMaximize;
+        FramebufferSize_t mFramebufferSize;
+        WindowContentScale_t mWindowContentScale;
+        MouseButton_t mMouseButton;
+        CursorPos_t mCursorPos;
+        CursorEnter_t mCursorEnter;
+        Scroll_t mScroll;
+        Key_t mKey;
+        Char_t mChar;
+        Charmods_t mCharmods;
+        Drop_t mDrop;
+        Monitor_t mMonitor;
+        Joystick_t mJoystick;
+    };
+}
+#endif // CALLBACKDEFS_HPP
--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "TIMGE/Application.hpp"
 #include "TIMGE/Keyboard.hpp"
 #include "TIMGE/Mouse.hpp"
@@ -6,14 +5,6 @@
 #include <TIMGE/CallbackDefs.hpp>
 #include <TIMGE/TIMGE.hpp>
 
-=======
-#include <TIMGE/CallbackDefs.hpp>
-#include <TIMGE/TIMGE.hpp>
-
-#include <cstdio>
-#include <format>
-#include <cstdlib>
->>>>>>> master
 #include <GLFW/glfw3.h>
 
 #include <stdexcept>
@@ -31,44 +22,16 @@ class Game : public Application
     void Update();
     void Render();
 
-<<<<<<< HEAD
-    friend void CursorPosCallback(const V2d& cursorPosition);
     friend void KeyCallback(Key key, int scancode, TIMGE::Keyboard::Action action, Modifier mods);
     friend void DropCallback(int pathCount, const char* path[]);
-=======
-    friend void CursorPosCallback(double xPos, double yPos);
-    friend void KeyCallback(int key, int scancode, int action, int mods);
->>>>>>> master
 
     private:
         static Game* mInstance;
         static Game* GetInstance();
 };
 
-<<<<<<< HEAD
-void CursorPosCallback(const V2d& cursorPosition);
 void KeyCallback(Key key, int scancode, TIMGE::Keyboard::Action action, Modifier mods);
 void DropCallback(int pathCount, const char* path[]);
-=======
-void CursorPosCallback(double xPos, double yPos);
-void KeyCallback(int key, int scancode, int action, int mods);
-
-Window::Info win_info = 
-{
-    "Callbacks", /*mTitle*/
-    1000, /*mWidth*/
-    500, /*mHeight*/
-    200, /*mMinWidth*/
-    100, /*mMinHeight*/
-    0, /*mMaxWidth*/
-    0, /*mMaxHeight*/
-    3, /*mOpenGLVersionMajor*/
-    3, /*mOpenGLVersionMinor*/
-    671, /*mFlags*/
-    "/home/marcel/Dev/C++/TIMGE/Sandbox/resources/youtube_logo.png", /*mIconPath*/
-    false, /*mIsFullscreen*/
-};
->>>>>>> master
 
 Vector<float, 4> color = {1.0f, 0.63f, 0.1f, 1.0f};
 
@@ -77,7 +40,6 @@ Callback::Callbacks callbacks{};
 Game* Game::mInstance = nullptr;
 
 Game::Game()
-<<<<<<< HEAD
  : Application(
          {
             {
@@ -105,15 +67,11 @@ Game::Game()
             },
             callbacks
          })
-=======
- : Application({win_info, color, callbacks})
->>>>>>> master
 {
     if (Game::mInstance) {
         throw "Only one instance of Game is allowed!\n";
     }
     mInstance = this;
-<<<<<<< HEAD
 
     Mouse& mouse = GetMouse();
 
@@ -131,8 +89,6 @@ Game::Game()
     // mouse.DeleteCursor(*cursors[1]);
     //
     // mouse.SetCursor(*cursors[1]);
-=======
->>>>>>> master
 }
 
 Game* Game::GetInstance() {
@@ -140,7 +96,6 @@ Game* Game::GetInstance() {
 }
 void Game::Run()
 {
-<<<<<<< HEAD
     Window& window = GetWindow();
 
     while (!window.ShouldClose()) {
@@ -150,49 +105,11 @@ void Game::Run()
              Render();
          }
          Application::EndFrame();
-=======
-   while (!Application::WindowShouldClose()) {
-        Application::BeginFrame();
-        {
-            Update();
-            Render();
-        }
-        Application::EndFrame();
->>>>>>> master
     }
 }
 
 void Game::Update()
 {
-<<<<<<< HEAD
-=======
-    //GetWindow().SetAspectRatio(21, 9);
->>>>>>> master
-    // if (glfwGetKey(GetWindow().GetWindow(), GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    //     glfwSetWindowShouldClose(GetWindow().GetWindow(), 1);
-    // } else if (glfwGetKey(GetWindow().GetWindow(), GLFW_KEY_F11) == GLFW_PRESS)
-    // {
-    //     static bool isMaximized = false;
-    //     if (!isMaximized) {
-    //     Application::GetWindow().Maximize();
-    //     } else {
-    //         GetWindow().Restore();
-    //     }
-    //
-    //     isMaximized = !isMaximized;
-    // } else if (glfwGetKey(GetWindow().mWindow, GLFW_KEY_F8) == GLFW_PRESS)
-    // {
-    //     static bool isHidden = false;
-    //     if (!isHidden) {
-    //         GetWindow().Hide();
-    //     } else {
-    //         GetWindow().Show();
-    //     }
-    //
-    //     isHidden = !isHidden;
-    // } else if (glfwGetKey(GetWindow().mWindow, GLFW_KEY_F12) == GLFW_PRESS) {
-    //     GetWindow().Fullscreen();
-    // }
 }
 
 void Game::Render()
@@ -202,9 +119,7 @@ void Game::Render()
 
 int main()
 {
-    callbacks.mCursorPos = CursorPosCallback;
     callbacks.mKey = KeyCallback;
-<<<<<<< HEAD
     callbacks.mDrop = DropCallback;
 
     try {
@@ -215,10 +130,6 @@ int main()
     } catch (const char* e) {
         std::cerr << "Something went wront: " << e << "!\n";
     }
-}
-
-void CursorPosCallback(const V2d& cursorPosition)
-{
 }
 
 void KeyCallback(Key key, int scancode, TIMGE::Keyboard::Action action, Modifier mods)
@@ -271,21 +182,5 @@ void DropCallback(int pathCount, const char* path[]) {
 
     for (int i = 0; i < pathCount; i++) {
         printf("Path: %s\n", path[i]);
-    }
-=======
-    Game game;
-    game.Run();
->>>>>>> master
-}
-
-void CursorPosCallback(double xPos, double yPos)
-{
-    Game::GetInstance()->GetWindow().SetTitle(std::format("x = {}, y = {}", xPos, yPos));
-}
-
-void KeyCallback(int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(Game::GetInstance()->GetWindow().GetWindow(), 1);
     }
 }
